0e096b3b6cf900f18e0c9e8f5ff59f969aa5a57d
jcesargm <juliocesar.gm6000@gmail.com>
2022-12-30 00:15:33 -0300

    Adicionando novas aulas e exercicios

---
+++ eloquente/Triangulo.js
@@ -0,0 +1,5 @@
+   var JogoDaVelha = "#";
+  for(var counter = 0; counter < 7; counter++){
+   console.log(JogoDaVelha); 
+   JogoDaVelha += "#";
+  } 
---
+++ eloquente/Xadrez.js
@@ -0,0 +1,7 @@
+
+  for(JogoDaVelha = 0; JogoDaVelha <= 4; JogoDaVelha++){
+  for(JogoDaVelha = 0; JogoDaVelha <= 4; JogoDaVelha++){
+  console.log("#");
+} 
+  console.log("# ");
+}
