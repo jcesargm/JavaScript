917ef59ccfde75fc974527f856ef901d6822a5dc
jcesargm <juliocesar.gm6000@gmail.com>
2023-01-05 15:12:52 -0300

    adding new exercises

---
+++ Curso JS Udemy/js/aula25/string.js
@@ -0,0 +1,6 @@
+/* window.document.body.innerHTML -> mostra o html da página (no console do google)
+ porém eu posso alterar aquilo: window.document.body.innerHTML = "Qualquer outra coisa" 
+mas toda vez que eu faço isso ele some com oq tava antes dessa nova string deixando somente ele, para solucionar isso
+para sempre incrementar ou seja continuar colocando coisas ali dentro: document.body.innerHTML += 'Escrever'; usando +=
+vai incrementar e será escrito no final da frase. Outro problema é que está na mesma linha
+ */
---
+++ Curso JS Udemy/js/aula27/math.js
@@ -0,0 +1,27 @@
+let num1 = 9;
+
+// console.log(100 / 0) // resulta em infinity e é true
+
+// console.log(!!(100 / 0)); // para ver o valor booleano
+
+// console.log(num1 ** (1/2)); // para fazer a raiz ou pode fazer tambem num1 ** 0.5
+
+// let num2 = Math.floor(num1); // arredondando para baixo
+// let num2 = Math.ceil(num1); // arredondando para cima
+// let num2 = Math.round(num1); // arredondando para o mais próximo
+
+// console.log(Math.max(1,2,3,4,5,-10,50)); // acha o maior número descrito ali
+// console.log(Math.min(1,2,3,4,5,-10,50)); // acha o menor número descrito ali
+
+// Math.PI // valor de pi
+// Math.pow(2, 1o) -> 2 elevado a 10 // potenciação
+// 2 ** 10 -> potenciação mais facil
+
+// console.log(Math.random()); // números aleatorios entre 0 e 1
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5) ; //(maximo - minimo)
+// console.log(2 ** 10); 
+
+
+
+
+
