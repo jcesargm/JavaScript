af07fca1ffa87828da80a21a27928e884c511a4e
jcesargm <juliocesar.gm6000@gmail.com>
2023-12-24 13:12:45 -0300

    removing folders

--- cursos/eloquente/Triangulo.js
+++
@@ -1,5 +0,0 @@
-   var JogoDaVelha = "#";
-  for(var counter = 0; counter < 7; counter++){
-   console.log(JogoDaVelha); 
-   JogoDaVelha += "#";
-  } 
--- cursos/eloquente/Xadrez.js
+++
@@ -1,7 +0,0 @@
-
-  for(JogoDaVelha = 0; JogoDaVelha <= 4; JogoDaVelha++){
-  for(JogoDaVelha = 0; JogoDaVelha <= 4; JogoDaVelha++){
-  console.log("#");
-} 
-  console.log("# ");
-}
